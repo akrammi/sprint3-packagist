3364abadfc9ea7c1c2cc052cc5d1a226489fecc0
Akram Moumen El Idrissi <a.elidrissi@void.fr>
2025-03-10 11:10:48 +0000

    Added imports

--- src/Configuration.php
+++ src/Configuration.php
@@ -2,5 +2,27 @@
 
     declare(strict_types= 1);
 
+    /*
+     * This file is part of the league/config package.
+     * 
+     * (c) Colin O'Dell <colinodell@gmail.com>
+     * 
+     * For the full copyright and license information, please view the LICENSE
+     * file that was distributed with this source code.
+    */
+
+    namespace League\Config;
+
+    use Dflydev\DotAccessData\Data;
+    use Dflydev\DotAccessData\Exception\DataInterface;
+    use Dflydev\DotAccessData\Exception\DataException;
+    use Dflydev\DotAccessData\Exception\InvalidPathException;
+    use League\Config\Exception\UnknownOptionException;
+    use League\Config\Exception\ValidationException;
+    use Nette\Schema\Expect;
+    use Nette\Schema\Processor;
+    use Nette\Schema\Schema;
+    use Nette\Schema\ValidationException as NetteValidationException;
+    
 
 ?>
